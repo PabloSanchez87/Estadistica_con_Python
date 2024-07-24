761de50864ea426746d35d07bc922cc8db577fa9
PabloSanchez <sancheztorrespablo@gmail.com>
2024-07-24 20:14:45 +0200

    Graficos y .py de creación de gráficos

--- Estadistica/seaborn_data_set_iris.py
+++ Estadistica/seaborn_data_set_iris.py
@@ -10,6 +10,7 @@ datos = sns.load_dataset('iris')
 # Imprimimos los datos
 print(datos)
 
+
 #! -----------------------------------------------------------------------------------
 #? Gráfico de dispersión
 
@@ -48,3 +49,81 @@ if not os.path.exists(output_path):
 
 
 #! -----------------------------------------------------------------------------------
+#? Gráfico de histograma
+
+output_path = "./Estadistica/graficos/grafico_histograma_iris.png"
+
+if not os.path.exists(output_path):
+    sns.histplot(datos['sepal_length'],bins=10)
+    plt.title('Histograma de LONGITUD del sépalo')
+    plt.xlabel('sepal_length')
+    plt.ylabel('Numero repeticiones')
+
+    # Guardamos el gráfico como imagen
+    plt.savefig(output_path)
+
+    # Mostramos el gráfico (si es posible, depende donde estés ejecutando el script.)
+    plt.show()
+
+
+#! -----------------------------------------------------------------------------------
+#? Gráfico de violín
+
+output_path = "./Estadistica/graficos/grafico_violin_iris.png"
+
+if not os.path.exists(output_path):
+    sns.violinplot(data=datos, x='species', y='sepal_length')
+    plt.title('Gráfico de violín')
+    plt.xlabel('species')
+    plt.ylabel('sepal_length')
+
+    # Guardamos el gráfico como imagen
+    plt.savefig(output_path)
+
+    # Mostramos el gráfico (si es posible, depende donde estés ejecutando el script.)
+    plt.show()
+    
+    
+#! -----------------------------------------------------------------------------------
+#? Gráfico de cajas
+
+# Definir la ruta donde se guardará el gráfico
+output_path = "./Estadistica/graficos/grafico_cajas_iris.png"
+
+# Verificar si el gráfico ya existe para evitar sobreescribir
+if not os.path.exists(output_path):
+    # Crear el gráfico de cajas
+    sns.boxplot(data=datos, x='species', y='sepal_length')
+    plt.title('Gráfico de cajas de longitud del sépalo por especie')
+    plt.xlabel('Species')
+    plt.ylabel('Sepal Length')
+
+    # Guardar el gráfico como imagen
+    plt.savefig(output_path)
+
+    # Mostrar el gráfico
+    plt.show()
+
+
+#! -----------------------------------------------------------------------------------
+#? Heatmap
+
+output_path = "./Estadistica/graficos/grafico_heatmap_iris.png"
+
+if not os.path.exists(output_path):    
+    # # Pairplot
+    sns.pairplot(data=datos, hue='species')
+    plt.show()
+    
+    #Heatmap
+    # Seleccionar solo las columnas numéricas para calcular la matriz de correlación
+    correlacion = datos.drop(columns=['species']).corr()
+    
+    sns.heatmap(correlacion, annot=True, cmap='coolwarm')
+    plt.title('Mapa de calor de las correlaciones')
+
+    # Guardamos el gráfico como imagen
+    plt.savefig(output_path)
+
+    # Mostramos el gráfico (si es posible, depende donde estés ejecutando el script.)
+    plt.show()
